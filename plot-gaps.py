d44a669246f9f2f458b93b53768cbf3f91de4736
Lucas Dias <lucasccdias0@gmail.com>
2021-02-03 16:14:12 -0300

    add nomes no mapa

--- plot-gaps.py
+++ plot-gaps.py
@@ -14,13 +14,12 @@ os.chdir('C:/Users/Lucas/Desktop/rais-wage-gap')
 
 wage_gaps = pd.read_csv('tmp/wage_gaps.csv')
 
-
 with open('input/sp_simple3.geojson', encoding='utf-8') as response:    
     mapa_mun_sp = json.load(response)
 
+nomes = pd.read_csv('input/sp_hard2.csv', encoding = 'utf-8')
 
-
-
+wage_gaps = wage_gaps.merge(nomes, left_on = 'Mun', right_on = 'code' )
 
 # Create figure
 fig = go.Figure()
@@ -30,6 +29,7 @@ fig.add_trace(go.Choropleth(geojson = mapa_mun_sp,
                             featureidkey = "properties.CD_GEOCMU",
                             locations = wage_gaps['Mun'],
                             z = wage_gaps['gap_negros'],
+                            text = wage_gaps['name'],
                             colorscale = "Viridis",
                             colorbar_title = "Diferença salarial <br> (negros - brancos)",
                             marker_line_color = 'white',
@@ -41,7 +41,8 @@ fig.update_layout(
         go.layout.Annotation(x = 0.5,
                              y = -0.1,
                              text = ("Diferença no log(salário/h) entre negros e brancos controlando por idade,"
-                             "ocupação, setor e sexo. <br> Cálculos a partir dos microdados da RAIS 2019."
+                             "ocupação, setor e sexo. <br> Cálculos a partir dos microdados da RAIS 2019. <br>"
+                             'Valores < 0 indicam salários menores para negros quando comparados a brancos.'
                              "Coeficientes não significativos a 10% foram considerados zero."),                   
                              showarrow = False, xref='paper', yref='paper', 
                              xanchor='center',
@@ -69,6 +70,7 @@ fig.add_trace(go.Choropleth(geojson = mapa_mun_sp,
                             featureidkey = "properties.CD_GEOCMU",
                             locations = wage_gaps['Mun'],
                             z = wage_gaps['gap_mulheres'],
+                            text = wage_gaps['name'],
                             colorscale = "Viridis",
                             colorbar_title = "Diferença salarial <br> (mulheres - homens)",
                             marker_line_color = 'white',
@@ -80,7 +82,8 @@ fig.update_layout(
         go.layout.Annotation(x = 0.5,
                              y = -0.1,
                              text = ("Diferença no log(salário/h) entre mulheres e homens controlando por idade,"
-                             "ocupação, setor e raça. <br> Cálculos a partir dos microdados da RAIS 2019."
+                             "ocupação, setor e raça. <br> Cálculos a partir dos microdados da RAIS 2019. <br>"
+                             'Valores < 0 indicam salários menores para mulheres quando comparadas aos homens.'
                              "Coeficientes não significativos a 10% foram considerados zero."),                   
                              showarrow = False, xref='paper', yref='paper', 
                              xanchor='center',
